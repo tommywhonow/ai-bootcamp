25157f0060a9517b1c2acbd214e3efc130debf74
tommywhonow <511897137@qq.com>
2026-05-03 11:15:32 +0800

    day 1 - variables and type hints

---
+++ day1_basics.py
@@ -0,0 +1,9 @@
+name: str="AI Engineer"
+year: int=2026
+hours_per_week: int = 40
+
+print(f"starting boot camp in {year}")
+print(f"target role: {name}")
+print(f"Hours per week: {hours_per_week}")
+
+
